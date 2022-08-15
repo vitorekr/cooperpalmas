d9f8cb9aa3e87dee3bfa1b655a85765c546cd9eb
vitor <vitorekrlol@gmail.com>
2022-08-15 10:36:47 -0300

    Update CooperPalmas

--- cooperPalmas/cooperPalmas/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django_nextjs.render import render_nextjs_page_sync
-
-#def index(request):
- #   return HttpResponse("Hello, world. You're at the polls index.")
-
-
-def index(request):
-    return render_nextjs_page_sync(request)

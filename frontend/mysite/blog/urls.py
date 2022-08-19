579d88b73ece081dbf304ba0d7414cbde4d6d664
vitor <vitorekrlol@gmail.com>
2022-08-18 22:51:59 -0300

    url update

--- frontend/mysite/blog/urls.py
+++ frontend/mysite/blog/urls.py
@@ -4,7 +4,11 @@ from django.conf.urls.static import static
 from django.urls import path
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
+    path('', views.land_page, name='landPage'),
+    path('contato/', views.contato, name='contato'),
+    path('regimentos/', views.contato, name='regimentos'),
+    path('blog/', views.PostList.as_view(), name='home'),
+    path('recent/', views.testAA, name='recents'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-    path('<slug:slug>/', views.category, name='category_detail')
+    path('<slug:slug>/', views.category, name='category_detail'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

86cf3f5273b778dec18c92ae8bf56d1c93ed4b7c
PedroMurta <pedromurta.7@gmail.com>
2022-08-24 18:04:36 -0300

    ta na mão otaro denovo

--- frontend/mysite/blog/urls.py
+++ frontend/mysite/blog/urls.py
@@ -1,10 +1,16 @@
 from . import views
+from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
+    path('admin/', admin.site.urls, name='admin'),
+    path('', views.sobre, name='home'),
+    path('sobre/', views.contato, name='contato'),
+    path('regimentos/', views.regimentos, name='regimentos'),
+    path('blog/', views.PostList.as_view(), name='blog'),
+    path('recent/', views.testAA, name='recents'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-    path('<slug:slug>/', views.category, name='category_detail')
+    path('<slug:slug>/', views.category, name='category_detail'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- frontend/mysite/blog/views.py
+++ frontend/mysite/blog/views.py
@@ -3,16 +3,39 @@ from .models import Post, Category
 from django.shortcuts import get_object_or_404, redirect, render
 
 class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    queryset = Post.objects.filter(status=1).order_by('-created_on')   
     template_name = 'index.html'
+
 
 class PostDetail(generic.DetailView):
     model = Post
     template_name = 'post_detail.html'
-    
+
+# def postDetail(request):
+#     detail_list = Post.objects.filter(status=1).order_by('-created_on')
+#     context = {'detail_list': detail_list}
+#     return render(request, 'post_detail.html', context)
+
+def sobre(request):
+    return render(request, 'teste.html')
+
+def land_page(request):
+    return render(request, 'index.html')
+
+def regimentos(request):
+    return render(request, 'regimentos.html')
+
+def contato(request):
+    return render (request, 'contact.html')
+
+def testAA(request):
+    test_list = Post.objects.order_by('-created_on')[:3]
+    context = {'test_list': test_list}
+    return render(request, 'post_detail.html', context)
+
+
 
 def category(request, slug):
     category = get_object_or_404(Category, slug=slug)
-    posts = category.posts.filter(status=Post.ACTIVE)
-
+    posts = category.posts.filter(status=Post.ACTIVE)    
     return render(request, 'blog/category.html', {'category': category, 'posts': posts})

ba10d0eff93833a2ce8c3e32b0620c911b141de9
vitor <vitorekrlol@gmail.com>
2022-11-06 17:36:06 -0300

    update

--- frontend/mysite/blog/views.py
+++ frontend/mysite/blog/views.py
@@ -2,41 +2,79 @@ from django.views import generic
 from .models import Post, Category
 from django.shortcuts import get_object_or_404, redirect, render
 
+
 class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1).order_by('-created_on')   
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
+
 
 class PostListNews(generic.ListView):
-    queryset = Post.objects.filter(category_id=1) 
-    template_name = 'index.html'
+    queryset = Post.objects.filter(category_id=1)
+    if queryset:
+        template_name = 'index.html'
+    else:
+        queryset = Post.objects.filter(status=1).order_by('-created_on')
+        template_name = 'index.html'
+
 
 class PostListFeira(generic.ListView):
-    queryset = Post.objects.filter(category_id=2) 
-    template_name = 'index.html'
+    queryset = Post.objects.filter(category_id=2)
+    if queryset:
+        template_name = 'index.html'
+    else:
+        queryset = Post.objects.filter(status=1).order_by('-created_on')
+        template_name = 'index.html'
+
 
 class PostListFinanceiro(generic.ListView):
-    queryset = Post.objects.filter(category_id=3) 
-    template_name = 'index.html'
+    queryset = Post.objects.filter(category_id=3)
+    if queryset:
+        template_name = 'index.html'
+    else:
+        queryset = Post.objects.filter(status=1).order_by('-created_on')
+        template_name = 'index.html'
+
 
 class PostListTransparencia(generic.ListView):
-    queryset = Post.objects.filter(category_id=4) 
-    template_name = 'index.html'
+    queryset = Post.objects.filter(category_id=4)
+    if queryset:
+        template_name = 'index.html'
+    else:
+        queryset = Post.objects.filter(status=1).order_by('-created_on')
+        template_name = 'index.html'
+
 
 class PostListEventos(generic.ListView):
-    queryset = Post.objects.filter(category_id=5) 
-    template_name = 'index.html'
+    queryset = Post.objects.filter(category_id=5)
+    if queryset:
+        template_name = 'index.html'
+    else:
+        queryset = Post.objects.filter(status=1).order_by('-created_on')
+        template_name = 'index.html'
+
 
 class PostListInformativos(generic.ListView):
-    queryset = Post.objects.filter(category_id=6) 
-    template_name = 'index.html'
+    queryset = Post.objects.filter(category_id=6)
+    if queryset:
+        template_name = 'index.html'
+    else:
+        queryset = Post.objects.filter(status=1).order_by('-created_on')
+        template_name = 'index.html'
+
 
 class PostIndex(generic.ListView):
-    queryset = Post.objects.filter(status=1).order_by('-created_on')   
-    template_name = 'landing.html'
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    if queryset:
+        template_name = 'index.html'
+    else:
+        queryset = Post.objects.filter(status=1).order_by('-created_on')
+        template_name = 'index.html'
+
 
 class PostDetail(generic.DetailView):
     model = Post
     template_name = 'post_detail.html'
+
 
 # def postDetail(request):
 #     detail_list = Post.objects.filter(status=1).order_by('-created_on')
@@ -45,24 +83,29 @@ class PostDetail(generic.DetailView):
 '''
 def landing(request):
     return render(request, 'landing.html')'''
-   
+
 
 def sobre(request):
     return render(request, 'sobre.html')
 
+
 def land_page(request):
     return render(request, 'index.html')
+
 
 def regimentos(request):
     return render(request, 'regimentos.html')
 
+
 def estatuto(request):
     return render(request, 'estatuto.html')
 
+
 def contato(request):
-    return render (request, 'contact.html')
+    return render(request, 'contact.html')
+
 
 def category(request, slug):
     category = get_object_or_404(Category, slug=slug)
-    posts = category.posts.filter(status=Post.ACTIVE)    
+    posts = category.posts.filter(status=Post.ACTIVE)
     return render(request, 'blog/category.html', {'category': category, 'posts': posts})
